3336620ed7d7fc5e935c295ff793ba7d62980022
Luis Eduardo Gutiérrez Saenz Pardo <luiseduardo.gtz@outlook.com>
2020-08-15 02:53:54 -0500

    Mobile responsive nav

--- ifc/src/Components/Header/Header.js
+++ ifc/src/Components/Header/Header.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, {useState,useRef} from 'react'
 import Logo from '../Assets/LOGO.svg'
 import FB from '../Assets/FB.svg'
 import IG from '../Assets/IG.svg'
 import SH from '../Assets/SH.svg'
 import IFCO from '../Assets/IFConline.svg'
+import menu from '../Assets/menu.svg'
+import fb2 from '../Assets/fb1.svg'
+import ins2 from '../Assets/ins2.svg'
+import smt2 from '../Assets/smt2.svg'
+import ifc2 from '../Assets/ifc2.svg'
 import {NavLink} from "react-router-dom";
 
 import style from "./style.css"
-  
 
-export default (props)=>{
+export function Header(){
+    const [navShow, showNav] = useState(false);
+    const refNav = useRef(null);
 
+    const triggerNav =()=>{
+        refNav.current.classList.toggle('open');
+    }
+    
     return(
     <div className="NavContainer">
         <div className="logoContainer">
-        <a className="logo" href="/" ><img style={{margin: 20}} src={Logo} height="60" alt=""></img></a>
+        <a className="logo" href="/" ><img src={Logo} alt=""></img></a>
         </div>
 
         <div className="NavTagsContainer">
@@ -30,18 +40,41 @@ export default (props)=>{
         <div classNam="SocialIcons">
         <ul className="IconTags">
             <li><a target="_blank" href="https://www.facebook.com/Info-Center-Operadora-Mayorista-102966134525422">
-                <img src={FB} height="80" alt=""></img></a></li>
+                <img src={FB} alt=""></img></a></li>
             <li><a target="_blank" href="https://www.instagram.com/info_center_operadora/?hl=es-la">
-                <img src={IG} height="80" alt=""></img></a></li>
+                <img src={IG} alt=""></img></a></li>
             <li><a target="_blank" href="http://www.smarthotel.com.mx/">
-                <img src={SH} height="80" alt=""></img></a></li>
+                <img src={SH} alt=""></img></a></li>
             <li><a href="/ifconline">
-                <img src={IFCO} height="80" alt=""></img></a></li>
+                <img src={IFCO} alt=""></img></a></li>
         </ul>
 
         </div>
 
-        
+        <div className="menuTrigger">
+            <button onClick={()=>triggerNav()}><img src={menu} alt=""></img></button>
+        </div>
+
+        <div className="mobile-nav" ref={refNav}>
+            <ul className="mobile-tags">
+                <li><NavLink exact to="/">Inicio</NavLink></li>
+                <li><NavLink exact to="/nosotros">Nosotros</NavLink></li>
+                <li><NavLink exact to="/destinos">Destinos</NavLink></li>
+                <li><NavLink exact to="/agencias">Registro agencias</NavLink></li>
+                <li><NavLink exact to="/contacto">Contacto</NavLink></li>
+
+                <li><a target="_blank" href="https://www.facebook.com/Info-Center-Operadora-Mayorista-102966134525422">
+                    <img src={fb2} alt=""></img>Facebook</a></li>
+                <li><a target="_blank" href="https://www.instagram.com/info_center_operadora/?hl=es-la">
+                    <img src={ins2} alt=""></img>Instagram</a></li>
+                <li><a target="_blank" href="http://www.smarthotel.com.mx/"><img src={smt2} alt="">
+                    </img>SmartHotel</a></li>
+                <li><a href="/ifconline">
+                    <img src={ifc2} alt=""></img>IFCOnline</a></li>
+            </ul>
+        </div>      
     </div>
     )
 }
+
+export default Header;
