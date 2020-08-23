04678b9636f786531e59f367c2ff7208b9dce81b
Luis Eduardo Gutiérrez Saenz Pardo <luiseduardo.gtz@outlook.com>
2020-08-23 03:32:51 -0500

    Agency registry and Carousel redesign

--- ifc/src/Components/Agency/Agency.js
+++ ifc/src/Components/Agency/Agency.js
@@ -3,70 +3,80 @@ import React from 'react';
 import style from "./style.css"
 
 export default (props)=>{
-return(
-<div className="FormContainer">
-<div className="form-container">
-
-<form method="post" action="">
-    <div className="FormTitle">
-        <h4>Registro Agencia</h4>
-    </div>
-    <div className="formInputs">
-    <div className="inputFormDiv">
-        <div className="divInputs">
-            <label>Nombre de la empresa:</label><br/>
-            <input className="largeInput" placeholder="Nombre de la empresa"></input><br/>
-            <label>Nombre comercial:</label><br/>
-            <input className="largeInput" placeholder="Nombre comercial"></input><br/>
-        </div>
-    </div>
-
-    <div className="inputFormDiv">
-        <div className="divInputs">
-            <label>Página Web:</label><br/>
-            <input className="mediumInput" placeholder="Página Web"></input><br/>
-            <label>Calle:</label><br/>
-            <input className="mediumInput" placeholder="Calle"></input><br/>
-            <label>Colonia:</label><br/>
-            <input className="mediumInput" placeholder="Colonia"></input><br/>
-            <label>Ciudad:</label><br/>
-            <input className="mediumInput" placeholder="Ciudad"></input><br/>
-            <label>Estado:</label><br/>
-            <input className="mediumInput" placeholder="Estado"></input><br/>
-            <label>Teléfono 1:</label><br/>
-            <input className="mediumInput" placeholder="Teléfono 1"></input><br/>
-        </div>
-        <div className="divInputs">
-            <label>RFC:</label><br/>
-            <input className="mediumInput" placeholder="RFC"></input><br/>
-                <div className="middleDiv">
-                    <div className="numberDiv">
-                    <label>No. Exterior:</label><br/>
-                    <input className="smallInput" placeholder="No. Exterior"></input><br/>
+    return(
+        <div className="form-container">
+            <form method="post" action="">
+                <div className="FormTitle">
+                    <h4 className="bold">Registro Agencia</h4>
+                </div>
+                <div className="formInputs">
+                    <div>
+                        <label>Nombre de la empresa:</label><br/>
+                        <input placeholder="Nombre de la empresa"></input><br/>
                     </div>
-                    <div  className="numberDiv">
-                    <label>No. Interior:</label><br/>
-                    <input className="smallInput" placeholder="No. Interior"></input><br/>
+                    <div>
+                        <label>Nombre comercial:</label><br/>
+                        <input placeholder="Nombre comercial"></input><br/>
+                    </div>
+                    <div>
+                        <label>RFC:</label><br/>
+                        <input placeholder="RFC"></input><br/>
+                    </div>
+                    <div>
+                        <label>Página Web:</label><br/>
+                        <input placeholder="Página Web"></input><br/>
+                    </div>
+                    <div>
+                        <label>Calle:</label><br/>
+                        <input placeholder="Calle"></input><br/>
+                    </div>
+                    <div>
+                        <label>Colonia:</label><br/>
+                        <input placeholder="Colonia"></input><br/>
+                    </div>
+                    <div>
+                        <label>Ciudad:</label><br/>
+                        <input placeholder="Ciudad"></input><br/>
+                    </div>
+                    <div>                  
+                        <label>Estado:</label><br/>
+                        <input placeholder="Estado"></input><br/>
+                    </div>
+               
+                    <div>
+                        <label>No. Exterior:</label><br/>
+                        <input placeholder="No. Exterior"></input><br/>
+                    </div>
+                    <div>
+                        <label>No. Interior:</label><br/>
+                        <input placeholder="No. Interior"></input><br/>
+                    </div>
+                    <div>
+                        <label>Código Postal:</label><br/>
+                        <input placeholder="Código Postal"></input><br/>
+                    </div>
+                    <div>
+                        <label>Municipio:</label><br/>
+                        <input placeholder="Municipio"></input><br/>
+                    </div>
+                    <div>
+                        <label>País:</label><br/>
+                        <input placeholder="País"></input><br/>
+                    </div>
+                    <div>
+                        <label>Teléfono 1:</label><br/>
+                        <input placeholder="Teléfono 1"></input><br/>
+                    </div>
+               
+                    <div>
+                        <label>Teléfono 2:</label><br/>
+                        <input placeholder="Teléfono 2"></input><br/>
                     </div>
                 </div>
-            <label>Código Postal:</label><br/>
-            <input className="mediumInput" placeholder="Código Postal"></input><br/>
-            <label>Municipio:</label><br/>
-            <input className="mediumInput" placeholder="Municipio"></input><br/>
-            <label>País:</label><br/>
-            <input className="mediumInput" placeholder="País"></input><br/>
-            <label>Teléfono 2:</label><br/>
-            <input className="mediumInput" placeholder="Teléfono 2"></input><br/>
+                <div className="btn-agency">
+                    <input type="submit" value="ENVIAR" />
+                </div>
+            </form>
         </div>
-    </div>
-        <div class="submit-input" align="center">
-            <input id="submit-button" type="submit" value="ENVIAR" />
-        </div>
-    </div>
-</form>
-
-</div>
-</div>
-
 )
 }
--- ifc/src/Components/Carousel/Carousel.js
+++ ifc/src/Components/Carousel/Carousel.js
@@ -3,37 +3,45 @@ import React from 'react'
 import img1 from '../Assets/img1.jpg'
 import img2 from '../Assets/img2.jpg'
 import img3 from '../Assets/img3.jpg'
+import img4 from '../Assets/img4.jpg'
 
 
 
 
-export default (props)=>{
+export function Carousel(){
     return(
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={img1} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={img2} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={img3} class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="10000" data-pause="hover">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={img1} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={img2} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={img3} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={img4} class="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     )
 }
+
+export default Carousel;
